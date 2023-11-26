1a0fbf73668c11b6169f6e2e425fe8cd661e3001
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-26 01:18:00 -0700

    hompage gallery and header

---
+++ src/lib/home-gallery/HomeGallery.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+<section class="flex flex-row align-center">
+<section class="grid grid-cols-4 md:grid-cols-2 gap-2 m-10">
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/d-w-Q4vWchoavUs-unsplash.jpg" alt="crochet animal toy">
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/oscar-helgstrand-F68sBbY7p3I-unsplash.jpg" alt=" crocheted gingerbread stuffie" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/dario-gomes-lher3yguqVA-unsplash.jpg" alt="crocheted cactus stuffie" />
+		</div>
+	</div>
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/jacqueline-liban-3ugRoVpotNk-unsplash.jpg" alt="crocheted white throw pillow with lemons" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/kiy-turk-DdQSizMyrw0-unsplash.jpg" alt="white,blue,yellow and green granny square blanket" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="src/lib/images/merylove-art-zWWDco7uCu0-unsplash.jpg" alt="grey crochet puff stitch hat" />
+		</div>
+	</div>
+</section>
+</section>
