20babb22acfe99599f9bb63fb26d540b4c34b382
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-27 20:09:57 -0700

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.postcss';
 	import Navbar from '../lib/navbar/Navbar.svelte';
+	import Footer from '../lib/footer/Footer.svelte';
 </script>
 <Navbar></Navbar>
 <slot />
+<Footer></Footer>
