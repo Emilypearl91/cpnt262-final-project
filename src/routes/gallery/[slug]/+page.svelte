3d1a7833a2156f08510893a7a5146862c2ec4104
Emily Pearl <Emilypearl9193@gmail.com>
2023-12-01 23:35:36 -0700

    description padding

--- src/routes/gallery/[slug]/+page.svelte
+++ src/routes/gallery/[slug]/+page.svelte
@@ -5,5 +5,5 @@
 <section>
 <h1 class="h1 m-2 text-center p-4">{data.post.title}</h1>
 <img src="{data.post.img}" alt="{data.post.alt}" class="w-1/2 m-auto">
-<p class="text-center">{data.post.description}</p>
+<p class="text-center p-2">{data.post.description}</p>
 </section>
