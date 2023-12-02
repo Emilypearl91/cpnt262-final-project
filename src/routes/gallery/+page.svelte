0a8e3d4feca27abef8d0ad6d1d1df2d65b1e3cc7
Emily Pearl <Emilypearl9193@gmail.com>
2023-12-01 23:23:32 -0700

    pattern page cards

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let data;
+  export let data;
 </script>
 
-<h1>patterns</h1>
+<h1 class="h1 text-center p-4">Patterns</h1>
 
 <ul>
-	{#each data.summaries as { slug, title }}
-		<li><a href="./gallery/{slug}">{title}</a></li>
-	{/each}
+<section class="flex flex-row flex-wrap justify-evenly">
+{#each data.summaries as { slug, title, img, alt }}
+	<li>		
+	<a href="./gallery/{slug}"> 
+		<div class="card p-4 max-w-md m-5">
+				<h2 class="h2 text-center p-4">{title}</h2> 
+				<img src="{img}" alt="{alt}" class="w">
+			</div>
+		</a> 
+	</li>
+{/each}	
+</section>
 </ul>
